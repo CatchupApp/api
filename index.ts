45175e4566a21cf6c4020200547888e4434a50c9
Akash Bhave <akashbbhave@gmail.com>
2020-10-03 13:14:42 -0400

    Install and configure Mongoose

--- index.ts
+++ index.ts
@@ -1,6 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+const PORT = process.env.PORT || 8000;
+const DB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/catchup";
+
 const app = express();
-const PORT = 8000;
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log(`⚙️  Connected to database at ${DB_URI}`);
+  })
+  .catch((err) => {
+    console.error(`⚙️  Failed to connect to database: ${err}`);
+  });
+
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => res.send("Express and TypeScript server"));
 app.listen(PORT, () => {
